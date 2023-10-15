9497c1b27e90ed2d345c07957d23c9855e89d2f7
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-10-16 02:00:41 +0400

    errorModal done

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import "./styles/base.scss";
 
 import Header from "./components/Header/Header";
 import GridCalendar from "./components/GridCalendar/GridCalendar";
+import ErrorModal from "./components/Modals/ErrorModal";
 
 function App() {
     moment.updateLocale("ru", { week: { dow: 1 } });
     const today = moment();
     const startDay = today.startOf("month").startOf("week").day(1);
-    
+
     const [currentMonth, setCurrentMonth] = useState(moment());
+    const [isError, setIsError] = useState(false);
 
     const changeMonth = (direction) => {
-        setCurrentMonth(currentMonth.clone().add(direction, 'months'));
+        setCurrentMonth(currentMonth.clone().add(direction, "months"));
+    };
+
+    const handleError = () => {
+        setIsError(true);
     };
 
     // window.moment = moment;
@@ -30,6 +36,8 @@ function App() {
                         .startOf("week")
                         .day(1)}
                 />
+                <ErrorModal isError={isError} setIsError={setIsError} />
+                <button onClick={handleError}>Вызвать ошибку</button>
             </div>
         </div>
     );
---
+++ src/components/Modals/ErrorModal.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Modal from "react-modal";
+
+import st from "./styles.module.scss";
+import close from '/icons/close.svg';
+import pigeon from '/img/pigeon.png';
+
+const ErrorModal = ({ isError, setIsError }) => {
+    console.log(isError);
+    const closeModal = () => {
+        setIsError(false);
+        console.log(isError);
+    };
+
+    const reloadPage = () => {
+        window.location.reload();
+    };
+
+    return (
+        <Modal
+            isOpen={isError}
+            onRequestClose={closeModal}
+            contentLabel="Error Modal"
+            ariaHideApp={false}
+            closeTimeoutMS={800}
+            overlayClassName={st.overlayWrapper}
+            className={st.modalWrapper}
+        >
+            <button className={st.modalCloseBtn} onClick={closeModal}>
+                <img src={close} />
+            </button>
+            <h2 className={st.h2}>
+                Что-то пошло
+                <br />
+                не так
+            </h2>
+            <h4 className={st.h4}>Попробуйте позже</h4>
+            <div className={st.decoration}>
+                <img src={pigeon} />
+            </div>
+            <button className={st.button} onClick={reloadPage}>
+                Хорошо
+            </button>
+        </Modal>
+    );
+};
+
+export default ErrorModal;
