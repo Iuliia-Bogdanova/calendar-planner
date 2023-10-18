384a2e705f1804ce21428126c348f09016ca2681
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-10-18 23:02:56 +0400

    events doing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import moment from "moment";
 
 import "./styles/base.scss";
@@ -7,10 +7,14 @@ import Header from "./components/Header/Header";
 import GridCalendar from "./components/GridCalendar/GridCalendar";
 import ErrorModal from "./components/Modals/ErrorModal/ErrorModal";
 
+const url = "http://localhost:3000/";
+
 function App() {
     moment.updateLocale("ru", { week: { dow: 1 } });
     const monthDates = moment();
     const startDay = monthDates.startOf("month").startOf("week").day(1);
+
+    window.moment = moment;
 
     const [currentMonth, setCurrentMonth] = useState(moment());
     const [isError, setIsError] = useState(false);
@@ -21,9 +25,24 @@ function App() {
 
     const handleError = () => {
         setIsError(true);
-    };
+    }; 
 
-    // window.moment = moment;
+    const [events, setEvents] = useState([]);
+    const startDateQuery = startDay.clone().format('X');
+    const endDateQuery = startDay.clone().add(42, 'days').format("X");
+
+    useEffect(() => {
+        fetch(
+            // `${url}/events/`
+            "http://localhost:3000/events/"
+            // `${url}/events?date_gte=${startDateQuery}&date_lte=${endDateQuery}`
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                console.log("Response", res);
+                setEvents(res);
+            });
+    }, []);
 
     return (
         <div className="App">
@@ -37,7 +56,9 @@ function App() {
                         .day(1)}
                 />
                 <ErrorModal isError={isError} setIsError={setIsError} />
-                <button className='button' onClick={handleError}>Вызвать ошибку</button>
+                <button className="button" onClick={handleError}>
+                    Вызвать ошибку
+                </button>
             </div>
         </div>
     );
