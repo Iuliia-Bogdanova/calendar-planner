6c9fb84891cdd147ee6fd8a5a4bbaacf91561a71
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-10-19 16:21:36 +0400

    AuthorizationModal doing + handleClearBtn

--- src/components/Modals/AuthorizationModal/AuthorizationModal.jsx
+++ src/components/Modals/AuthorizationModal/AuthorizationModal.jsx
@@ -6,9 +6,20 @@ import close from "/icons/close.svg";
 
 function AuthorizationModal({ isOpen, onRequestClose }) {
     const [email, setEmail] = useState("");
+    const [showClear, setShowClear] = useState(false);
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
+        if (event.target.value !== "") {
+            setShowClear(true);
+        } else {
+            setShowClear(false);
+        }
+    };
+
+    const handleClear = () => {
+        setEmail("");
+        setShowClear(false);
     };
 
     const handleSubmit = (event) => {
@@ -33,14 +44,26 @@ function AuthorizationModal({ isOpen, onRequestClose }) {
             <h3 className={st.h3}>Вход</h3>
             <form className={st.form} onSubmit={handleSubmit}>
                 <div className={st.mailForm}>
-                    <label className={st.label}>E-mail</label>
-                    <input
-                        className={st.inputMail}
-                        type="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                        placeholder=""
-                    />
+                    <div className={st.labelInput}>
+                        <label className={st.label}>E-mail</label>
+                        <input
+                            className={st.inputMail}
+                            type="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            placeholder=""
+                        />
+                    </div>
+                    {showClear && (
+                        <button
+                            className={
+                                st.modalCloseBtn + " " + st.modalCloseBtnSmall
+                            }
+                            onClick={handleClear}
+                        >
+                            <img src={close} />
+                        </button>
+                    )}
                 </div>
                 <button className={st.button} type="submit">
                     Далее
